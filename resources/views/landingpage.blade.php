a4aa3864ef80ef941ee1359f5b7b9e5be1cc25b4
Desy Ayurianti <desii.ayurianti@gmail.com>
2023-03-07 02:28:27 +0700

    edit

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,7 +5,7 @@
         <br><br>
                 <div class="card-header border-0">
                     <div class="d-flex justify-content-between">
-                        <h3> <b>DesyMaria's Website</b></h3>
+                        <h3> <b>Desy and Maria's Website</b></h3>
                     </div>
                 </div>
     </section>
